04ec854ee26a7fb5c1aa60359459e3e608067c17
erisamsai <103209376+erisamsai@users.noreply.github.com>
2022-09-12 12:48:09 +0100

    Update testing.py

--- brampton manor academy/2/testing.py
+++ brampton manor academy/2/testing.py
@@ -7,7 +7,9 @@ class MyFirstTests(unittest.TestCase):
 
     def test_richter_to_energy(self):
         self.assertEqual(richter_to_energy(1), 1995262.3149688789)
-
+        
+    def test_energy_to_tnt(self):
+        self.assertEqual(energy_to_tnt(1), 0.00047687913837688307)
 
 if __name__ == '__main__':
     unittest.main()
