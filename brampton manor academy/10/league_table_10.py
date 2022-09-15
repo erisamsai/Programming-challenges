0e49803bbf74a92a0dde1f14aa468da99015a072
erisamsai <103209376+erisamsai@users.noreply.github.com>
2022-09-15 13:24:49 +0100

    Update league_table_10.py

--- brampton manor academy/10/league_table_10.py
+++ brampton manor academy/10/league_table_10.py
@@ -16,39 +16,35 @@ def read_csv(csv_file):
                 csv_contents.append(row)
     return csv_contents
 
-def process_results(rows):
-    league_table = {}
-    for row in rows:
-        home, away, homegoals, awaygoals, winner = row[1], row[2], row[3], row[4], row[5]
-
-        if home not in league_table:
-         league_table[home] = [0,0]
-        if away not in league_table:
-         league_table[away] = [0,0]
-
-        if winner == "D":
-         league_table[home][0] += 1
-         league_table[away][0] += 1
-      
-        if winner == "H":
-         league_table[home] += 3
-         league_table[away] += 0
-       
-        if winner == "A":
-         league_table[home] += 0
-         league_table[away] += 3
-        
-        #goal difference also needs to be calculated
-         league_table['home'][1] = league_table[home][1] + homegoals - awaygoals
-        #goal difference for away team here
-         league_table['away'][1] = league_table[away][1] + awaygoals - homegoals
-        #after this is done, you'll have all of the points and goal differences for each team
-    return league_table
-
-
 if __name__ == "__main__":
     file_contents = read_csv(csv_file)
-    results = process_results(file_contents)
-    print(results)
-    #instead of printing, you'll need to sort function to sort...the league - this should help - https://stackoverflow.com/questions/1217251/sorting-a league_table-with-lists-as-values-according-to-an-element-from-the-list
-    #then print the league table in a pretty output function
+    league_table = {}
+
+    for row in file_contents:
+        home, away, home_goals, away_goals, winner = row[1], row[2], row[3], row[4], row[5]
+
+        if home not in league_table:
+            league_table[home] = [0, 0]
+        if away not in league_table:
+            league_table[away] = [0, 0]
+
+        if winner == 'D':
+            league_table[home][0] += 1
+            league_table[away][0] += 1
+
+        if winner == 'A':
+            league_table[home][0] += 0
+            league_table[away][0] += 3
+
+        if winner == 'H':
+            league_table[home][0] += 3
+            league_table[away][0] += 0
+
+        league_table[home][1] = int(league_table[home][1]) + int(home_goals) - int(away_goals)
+        league_table[away][1] = int(league_table[away][1]) + int(away_goals) - int(home_goals)
+
+    print(f"{'Team':<20} {'Points':<15} {'Goal Difference':<10}")
+    for key, value in sorted(league_table.items(), key=lambda e: e[1], reverse=True):
+        points, goal_difference = value
+        print(f'{key:<20} {points:<15} {goal_difference:<10}')
+
