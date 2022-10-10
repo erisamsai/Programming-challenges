9ebfeb4d8ddec3d8dde7e80627ac0e385c20249f
erisamsai <103209376+erisamsai@users.noreply.github.com>
2022-10-10 12:42:13 +0100

    Update scrape_singles.py

--- brampton manor academy/13/scrape_singles.py
+++ brampton manor academy/13/scrape_singles.py
@@ -9,38 +9,35 @@ def scrape(url):
     fp.close()
     return mystr
 
+
 if __name__ == "__main__":
     html = scrape(url)
 
-
 i = 0
+while 69990 > i > -1:
+    open_tr = html.find('<tr>', i)
+    close_tr = html.find("</tr>", open_tr)
+    newstring = html[open_tr:close_tr]
 
-while i > -1 :
- open_tr = html.find('<tr>', i)
- close_tr = html.find("</tr>", open_tr)
- newstring = html[open_tr:close_tr]
+    position = '"position">'
+    pos_start = newstring.find(position)
+    pos_end = newstring.find('</', pos_start)
+    actual_position = newstring[pos_start+len(position):pos_end]
+    print(actual_position)
 
 
- position = '"position">'
- pos_start = newstring.find(position)
- pos_end = newstring.find('</', pos_start)
- print(newstring[pos_start+len(position):pos_end])
- #actual_position = newstring[pos_start+len(position):pos_end]
+    title = 'alt="'
+    title_start = newstring.find(title)
+    title_end = newstring.find('" width', title_start)
+    actual_title = newstring[title_start+len(title):title_end]
+    print(actual_title)
 
- title = 'alt="'
- title_start = newstring.find(title)
- title_end = newstring.find('" width', title_start)
- print(newstring[title_start+len(title):title_end])
- #actual_title = newstring[title_start+len(title):title_end]
-
- artist = '<div class="artist">'
- url = '         <a href="/artist/'
- start_url = newstring.find(url)
- artist_start = newstring.find(artist)
- artist_end = newstring.find('</a', artist_start)
- end_url = newstring.find('/">', start_url)
- remove_url = end_url - start_url
- print(newstring[artist_start + len(artist) + len(url) + remove_url:artist_end])
- #actual_artist = newstring[artist_start + len(artist) + len(url) + remove_url:artist_end]
-
- i = close_tr
+    artist = '<div class="artist">'
+    url = '         <a href="/artist/'
+    start_url = newstring.find(url)
+    artist_start = newstring.find(artist)
+    artist_end = newstring.find('</a', artist_start)
+    end_url = newstring.find('/">', start_url)
+    remove_url = end_url - start_url
+    actual_artist = newstring[artist_start + len(artist) + len(url) + remove_url:artist_end]
+    print(actual_artist)
