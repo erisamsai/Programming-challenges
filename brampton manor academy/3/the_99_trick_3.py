326a06b040d5ce5bfb8e3c3b0d4ee34be578066a
erisamsai <103209376+erisamsai@users.noreply.github.com>
2022-10-23 15:59:39 +0100

    Update the_99_trick_3.py

--- brampton manor academy/3/the_99_trick_3.py
+++ brampton manor academy/3/the_99_trick_3.py
@@ -1,16 +1,36 @@
-print(' We are going to play a game.', '\n',
-      'I want you to pick a number then do a series of calculations.', '\n',
-      'I bet I know what the result of those calculations will be!')
+print(" We are going to play a game.",
+      '\n', "I want you to pick a number then do a series of calculations.",
+      '\n', "I bet I know what the result of those calculations will be!", '\n')
 
-answer = int(input('This will be the answer. Select a number between 10-49: '))
-player_number = int(input('Pick any number between 50-99: '))
 
-factor = 99 - answer
-result = factor + player_number
-result = (result - 100) + 1
-final_result = player_number - result
+def your_part():
+    answer = int(input(" This will be the answer. Select a number between 10-49: "))
+    return answer
 
-if final_result == answer:
-    print(f'I said the answer was {answer} and the calculation result is {final_result}')
-else:
-    print('error, try again')
+
+def friend_part():
+    friend_number = int(input(' Pick any number between 50-99: '))
+    return friend_number
+
+
+def calculation(answer, friend_number):
+    factor = 99 - answer
+    result = factor + friend_number
+    result = (result - 100) + 1
+    final_result = friend_number - result
+    return final_result
+
+
+def end_game(answer, final_result):
+    if final_result == answer:
+         print(f' I said the answer was {answer} and the calculation result is {final_result}.')
+    else:
+        print('error, try again')
+
+
+
+if __name__ == "__main__":
+    answer = your_part()
+    friend_number = friend_part()
+    final_result = calculation(answer, friend_number)
+    end_game(answer, final_result)
